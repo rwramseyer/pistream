d0cdf514c136fdcf0f5d1cb96151726424d408f0
Ryan Ramseyer <ramseyerrw@gmail.com>
2020-08-08 18:43:36 -0400

    added basic files and outlines

---
+++ pistream_client.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+# pistream_client.py
+# Allows client to connect to streaming device,
+# select stream, view stream options, and add/delete stream options
+
+import sockets
+
+# initialize connection
+
+
+# close connection
---
+++ pistream_server.py
@@ -0,0 +1,8 @@
+#!/usr/env/bin python3
+# pistream_server.py
+# This program runs on the pi and displays available
+# streams and allows stream selection
+
+import sockets
+
+
