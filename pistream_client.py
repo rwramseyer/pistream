934b35b6a6b09692609112f9617beca557afb8b9
Ryan Ramseyer <ramseyerrw@gmail.com>
2020-08-08 19:35:39 -0400

    progress on client and server

--- pistream_client.py
+++ pistream_client.py
@@ -3,9 +3,22 @@
 # Allows client to connect to streaming device,
 # select stream, view stream options, and add/delete stream options
 
-import sockets
+import socket
 
-# initialize connection
+HOST = 'pifour.local'
+PORT = 12019
 
+s = socket.socket()
+s.connect((HOST, PORT))
+print("Connected to:", (HOST, PORT))
 
-# close connection
+try:
+    command = input(" > ")
+    s.sendall(command)
+    
+    response = s.recv(8)
+    while "\n" not in data:
+        response += s.recv(8)
+    
+finally:
+    s.close()
--- pistream_server.py
+++ pistream_server.py
@@ -3,6 +3,94 @@
 # This program runs on the pi and displays available
 # streams and allows stream selection
 
-import sockets
+import socket
+import csv
+
+STREAM_FILE = "stream_options.csv"
+
+def change_stream(stream_id):
+    stream = get_current_streams()[stream_id]
+    print(stream)
+
+def get_current_streams():
+    # read csv and return list of streams in order
+    # expects csvs formatted as name,url
+    streams = []
+    with open(STREAM_FILE) as csvfile:
+        csv_reader = csv.reader(csvfile, delimiter=",")
+        line_count=0
+        for row in csv_reader:
+           print(row)
+
+    return streams 
+
+def accept_connections():
+
+    # init socket
+    s = socket.socket()
+    server_addr = ('localhost', 12019)
+    print("starting server on", server_addr)
+    s.bind(server_addr)
+    s.listen(1)
+
+    while True:
+        connection, client_addr = s.accept()
+        try:
+            print("connection from", client_addr)
+
+            message = "" 
+            while True:
+                data = connection.recv(1024)
+                if data:
+                    message += data
+                else:
+                    break
+
+            if message:
+                reply = execute_command(message)
+                connection.sendall(reply) 
+
+        finally:
+            connection.close()
+    
+def execute_command(message):
+    """Commands should come in form 'command argument'"""
+
+    command_list = ["help", "list", "add", "change", "delete"]
+    
+    msg_parts = message.split(" ")
+    command = msg_parts[0]
+    print("recieved command", command)
+    
+    if command == "list":
+        # send list of streams available and IDs for selection
+        pass
+    elif command == "change":
+        pass
+    elif command == "add":
+        # add given name and
+        pass
+    elif command == "delete":
+        pass
+    elif command == "help":
+        reply = "Commands\n"
+        reply += "list: lists all streams available"
+        reply += "change <stream_ID>: directs server to change active stream to specified"
+        reply += "add <stream_name> <URL>: adds new stream to permanent list"
+        reply += "delete <stream_ID>: deletes stream from permanent list"
+    else:
+        print("Received unknown command")
+        reply = "Error: unkown command"        
+
+    return reply
+     
+def main():
+    # start default stream
+    change_stream(1)
+
+    # wait for connections
+    accept_connections()
 
 
+if __name__ == "__main__":
+    main()
